2c280048c19fc581285a51e4d5c475aaef4937cc
Patrik Fritzner <Patrik@>
2013-09-20 00:05:51 +0200

    Workspace setup

---
+++ Client/gen/software_engineering/bal_ui/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package software_engineering.bal_ui;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Client/src/software_engineering/bal_ui/MainActivity.java
@@ -0,0 +1,40 @@
+package software_engineering.bal_ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	public final static String EXTRA_MESSAGE = "software_engineering.bal_ui.MESSAGE";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void mainMenuEnter (View view){
+		Intent intent = new Intent(this, DisplayLoginScreen.class); //next screen to go to
+    	startActivity(intent);
+	}
+	public void mainMenuSettings (View view){
+		Intent intent = new Intent(this, DisplaySettings.class); //next screen to go to
+    	startActivity(intent);
+	}
+	public void mainMenuInfo (View view){
+		Intent intent = new Intent(this, DisplayInfo.class); //next screen to go to
+    	startActivity(intent);
+	}
+	
+
+}
+//TESTTESTTESTTESTTESTTEST
